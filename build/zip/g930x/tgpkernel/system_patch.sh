fe4bec4c66a90fc93e311aeb318755720961a63f
djb77 <dwayne.bakewell@gmail.com>
2017-01-22 17:49:09 +1100

    More fixes to Wavelock
    Fixes to the installer script.
    Since init.d is no longer usable, the installer will now detect if
    SuperSU or PHH Superuser is available, and then copy the file to the
    proper location.
    
    Thanks to @ambasadii for his help
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build/zip/g930x/tgpkernel/system_patch.sh
+++ build/zip/g930x/tgpkernel/system_patch.sh
@@ -1,12 +1,12 @@
 #!/sbin/sh
-# Created by tkkg1994, modified by djb77
+# Created by tkkg1994, modified by ambasadii and djb77
 
-# Remove wrong build.prop entries
+# Remove wrong build.prop entries (@Tkkg1994)
 sed -i /timaversion/d /system/build.prop
 sed -i /security.mdpp.mass/d /system/build.prop
 sed -i /ro.hardware.keystore/d /system/build.prop
 
-# Setting correct fingerprint files
+# Setting correct fingerprint files (@Tkkg1994)
 rm -rf /system/app/TuiService /system/app/mcRegistry
 rm -f /system/vendor/lib/libsecure_storage.so
 rm -f /system/vendor/lib/libsecure_storage_jni.so
@@ -16,16 +16,25 @@ rm -f /system/vendor/lib64/libsecure_storage_jni.so
 # Remvoe old PersonalPageService APK
 rm -rf /system/priv-app/PersonalPageService
 
-# Copy Wavelock.sh to PHH Superuser (if exists)
-if [ ! -d /magisk/phh/su.d ];then
-	cp -f /tmp/tgpkernel/wavelock.sh /magisk/phh/su.d/wavelock.sh
-	chown 0:0 /magisk/phh/su.d/wavelock.sh
-	chmod 755 /magisk/phh/su.d/wavelock.sh
+# Install wavelock.sh if SuperSU is present (@ambasadii)
+if [ -e /su/su.d/97wakelock ]; then
+	chown 0:0 /su/su.d/97wakelock
+	chmod 700 /su/su.d/97wakelock
+else	
+	cp /tmp/tgpkernel/97wakelock /su/su.d/97wakelock
+	chown 0:0 /su/su.d/97wakelock
+	chmod 700 /su/su.d/97wakelock
 fi
 
-# Copy Wavelock.sh to SuperSU (if exists)
-if [ ! -d /su/su.d ];then
-	cp -f /tmp/tgpkernel/wavelock.sh /su/su.d/wavelock.sh
-	chown 0:0 /su/su.d/wavelock.sh
-	chmod 700 /su/su.d/wavelock.sh
+# Install wavelock.sh if PHH Superuser is present (@ambasadii)
+if [ -e /magisk/phh/su.d/97wakelock ]; then
+	chown 0:0 /magisk/phh/su.d/97wakelock
+	chmod 755 /magisk/phh/su.d/97wakelock
+else
+	mkdir /magisk/phh/su.d
+	find /magisk/phh/su.d -type d -exec chmod 755 {} \;
+	cp /tmp/tgpkernel/97wakelock /magisk/phh/su.d/97wakelock
+	chown 0:0 /magisk/phh/su.d/97wakelock
+	chmod 755 /magisk/phh/su.d/97wakelock
 fi
+
